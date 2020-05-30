15bd50b2e4e8f82fb18d1a25e491da733d33bd47
TOTH Benoit <47602460+Ahzenrei@users.noreply.github.com>
2020-05-30 22:21:00 +0200

    A lot of asset and some animation

--- Assets/Scripts/Actor.cs
+++ Assets/Scripts/Actor.cs
@@ -4,23 +4,50 @@ using UnityEngine;
 
 public class Actor : MonoBehaviour
 {
-
+    AgentBehavior agent;
     bool mouseOver;
+
+    Animator anim;
+    bool facingRight = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        agent = GetComponent<AgentBehavior>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 velocity = agent.aIPath.desiredVelocity;
+        Vector3 scale = transform.localScale;
+
+        anim.SetFloat("Velocity", Mathf.Abs(velocity.x) + Mathf.Abs(velocity.y));
+        if (velocity.x >= 0.01f && !facingRight)
+        {
+            facingRight = true;
+            scale.x *= -1;
+            transform.localScale = scale;
+        } else if (velocity.x <= -0.01f && facingRight)
+        {
+            facingRight = false;
+            scale.x *= -1;
+            transform.localScale = scale;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            agent.moveToDestination(worldPosition);
+        }
+        /*
         if (GameManager.canAct && mouseOver && Input.GetMouseButton(0))
         {
             GameManager.canAct = false;
             ChooseAction();
         }
+        */
     }
 
     private void OnMouseEnter()
--- Assets/Scripts/AgentBehavior.cs
+++ Assets/Scripts/AgentBehavior.cs
@@ -23,14 +23,36 @@ public class AgentBehavior : MonoBehaviour
         destinationSetter.target = targetPoint;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
-        if(Input.GetMouseButtonDown(0))
+        DrawPath();
+    }
+
+    void DrawPath()
+    {
+        List<Vector3> buffer = new List<Vector3>();
+        bool stale;
+        aIPath.GetRemainingPath(buffer, out stale);
+        if (!stale)
         {
-            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            moveToDestination(worldPosition);
+            List<Vector3> waypoints = new List<Vector3>();
+            Vector2 directionOld = Vector2.zero;
+
+            for (int i = 1; i < buffer.Count; i++) //Simplify the path
+            {
+                Vector2 directionNew = new Vector2(buffer[i - 1].x - buffer[i].x, buffer[i - 1].y - buffer[i].y);
+                if (directionNew != directionOld)
+                {
+                    waypoints.Add(buffer[i - 1]);
+                    directionOld = directionNew;
+                }
+            }
+
+            foreach(Vector3 waypoint in waypoints)
+            {
+                //Draw something to show pass
+            }
+
         }
     }
 
