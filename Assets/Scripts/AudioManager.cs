276ab47fb5a46db23085d728d0333232851d9b04
TOTH Benoit <47602460+Ahzenrei@users.noreply.github.com>
2020-05-31 19:01:04 +0200

    Waiter Movement, Assets, animation

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Audio {
+	public class AudioManager : MonoBehaviour {
+
+		public Sound[] sounds;
+		private static List<Sound> staticSounds;
+       
+		void Awake () {
+			staticSounds = new List<Sound>();
+ 
+			foreach (Sound s in sounds) {
+				s.source = gameObject.AddComponent<AudioSource>();
+				s.source.clip = s.clip;
+ 
+				s.source.loop = s.loop;
+				s.source.volume = s.volume;
+				s.source.pitch = s.pitch;
+
+ 
+				staticSounds.Add(s);
+			}       
+		}
+
+        public static void SetVolume(float volume)
+        {
+            foreach (Sound s in staticSounds)
+            {
+                s.source.volume = volume;
+            }
+        }
+
+        public static bool IsPlaying(string n)
+        {
+            foreach (Sound s in staticSounds)
+            {
+                if (s.name == n)
+                {
+                    return s.source.isPlaying;   
+                }
+            }
+            return false;
+        }
+
+        public static void Play(string n) {
+			foreach (Sound s in staticSounds) {
+				if (s.name == n) {
+					s.source.Play();
+					return;
+				}
+			}
+		}
+       
+		public static void Stop(string n) {
+			foreach (Sound s in staticSounds) {
+				if (s.name == n) {
+					s.source.Stop();
+					return;
+				}
+			}
+		}
+	}
+}
--- Assets/Scripts/WaiterActor.cs
+++ Assets/Scripts/WaiterActor.cs
@@ -5,44 +5,164 @@ using Pathfinding;
 
 public class WaiterActor : Actor
 {
-    public AIPath aIPath;
 
-    // Start is called before the first frame update
-    void Start()
+    [Header("Path")]
+    public GameObject pathA;
+    public GameObject pathB;
+    AIPath aIPath;
+    [SerializeField]
+    bool processingPath = false;
+    public GameObject pathChoosen;
+    public int index;
+
+    [Header("Entry")]
+    public float speedApparition = 1f;
+    public Transform startWaiter;
+    [SerializeField]
+    bool visible = false;
+    SpriteRenderer sprite;
+    Color color;
+
+    private void Start()
     {
+        anim = GetComponent<Animator>();
+        agent = GetComponent<AgentBehavior>();
+        gameManager = FindObjectOfType<GameManager>();
         aIPath = GetComponent<AIPath>();
+        sprite = GetComponent<SpriteRenderer>();
+        color = sprite.color;
+        sprite.color = new Color(0, 0, 0, 0); //Hidding the character
     }
 
     // Update is called once per frame
     void Update()
     {
-        DrawPath();
-    }
-
-    void DrawPath()
-    {
-        List<Vector3> buffer = new List<Vector3>();
-        bool stale;
-        aIPath.GetRemainingPath(buffer, out stale);
-        if (!stale)
+        /************** Temporary **************/
+        if (Input.GetKey(KeyCode.Space) && !processingPath)
         {
-            List<Vector3> waypoints = new List<Vector3>();
-            Vector2 directionOld = Vector2.zero;
-
-            for (int i = 1; i < buffer.Count; i++) //Simplify the path
+            CallWaiter();
+        }
+        /***************************************/
+        if (processingPath && aIPath.reachedDestination)
+        {
+            index++;
+            if (index < pathChoosen.transform.childCount)
             {
-                Vector2 directionNew = new Vector2(buffer[i - 1].x - buffer[i].x, buffer[i - 1].y - buffer[i].y);
-                if (directionNew != directionOld)
-                {
-                    waypoints.Add(buffer[i - 1]);
-                    directionOld = directionNew;
-                }
+                Vector3 target = pathChoosen.transform.GetChild(index).position;
+                MoveToTarget(target);
             }
-
-            foreach (Vector3 waypoint in waypoints)
+            else
             {
-                //Draw something to show pass
+                processingPath = false;
+                Debug.Log("You have arrived");
+                StartCoroutine("Disappear");
             }
         }
     }
+
+    public void CallWaiter()
+    {
+        visible = true;
+        processingPath = true;
+        index = 0;
+        StartCoroutine("Appear");
+        pathChoosen = (Random.Range(0, 1) == 0) ? pathA : pathB;
+        Vector3 target = pathChoosen.transform.GetChild(index).position;
+        MoveToTarget(target);
+    }
+
+    private void MoveToTarget(Vector3 target)
+    {
+        if (target != null)
+        {
+            agent.moveToDestination(target);
+        }
+        else
+        {
+            Debug.LogWarning("Child not found in path object");
+        }
+    }
+
+    private IEnumerator Appear()
+    {
+        transform.position = startWaiter.position;
+        Color temp = color;
+        temp.a = 0;
+        while (sprite.color.a < 1)
+        {
+            temp.a += speedApparition;
+            sprite.color = temp;
+            yield return null;
+        }
+        temp.a = 1;
+        sprite.color = temp;
+    }
+
+    private IEnumerator Disappear()
+    {
+        Color temp = color;
+        temp.a = 1;
+        while (sprite.color.a > 0)
+        {
+            temp.a -= speedApparition;
+            sprite.color = temp;
+            yield return null;
+        }
+        temp.a = 0;
+        sprite.color = temp;
+    }
 }
+
+
+
+/*
+private float Dst(Vector3 a, Vector3 b)
+  {
+      return Mathf.Pow(b.x - a.x, 2) + Mathf.Pow(b.y - a.y, 2);
+  }
+private void DrawPath()
+{
+    List<Vector3> buffer = new List<Vector3>();
+
+    aIPath.GetRemainingPath(buffer, out bool stale);
+
+    if (!stale)
+    {
+        drawingPath = true;
+        pathEffect.SetActive(true);
+        path = new List<Vector3>();
+        Vector2 directionOld = Vector2.zero;
+
+        for (int i = 1; i < buffer.Count; i++) //Simplify the path
+        {
+            Vector2 directionNew = new Vector2(buffer[i - 1].x - buffer[i].x, buffer[i - 1].y - buffer[i].y);
+            if (directionNew != directionOld)
+            {
+                path.Add(buffer[i - 1]);
+                directionOld = directionNew;
+            }
+        }
+        path.Add(aIPath.destination);
+        StartCoroutine("MoveEffectAlongPath", transform.position);
+
+    }
+}
+
+private IEnumerator MoveEffectAlongPath(Vector3 startPosition)
+{
+    pathEffect.transform.position = startPosition;
+    for (int i = 0; i < path.Count; i++)
+    {
+        float distance;
+        while ((distance = Dst(pathEffect.transform.position, path[i])) > 0.1f * 0.1f)
+        {
+            Vector3 direction = new Vector3(path[i].x - pathEffect.transform.position.x, path[i].y - pathEffect.transform.position.y, 0);
+            Vector3 velocity = direction * speedEffect * Time.deltaTime;
+            pathEffect.transform.Translate(velocity, Space.World);
+            yield return null;
+        }
+    }
+    drawingPath = false;
+    pathEffect.SetActive(false);
+}
+*/
